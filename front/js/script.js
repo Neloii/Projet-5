f46a494fb7b4fa12e9b739ab5e856da5ab950233
Vincent Forner <vincent.forner@hotmail.fr>
2022-07-28 14:27:59 +0200

    p1

---
+++ front/js/script.js
@@ -0,0 +1,23 @@
+//Pour node server :cd OneDrive\Documents\Projet 5\back
+
+//Recuperation de l'api
+fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((Articles) => {
+        console.table(Articles);
+        lesKanaps(Articles);
+    })
+
+//Afficher les articles
+function lesKanaps(index) {
+    let zoneArticle = document.querySelector("#items");
+    for(let article of index) {
+        zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+            <article>
+              <img src="${article.imageUrl}" alt="${article.altTxt}">
+              <h3 class="productName">${article.name}</h3>
+              <p class="productDescription">${article.description}.</p>
+            </article>
+          </a>`;
+    }
+}
